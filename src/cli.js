51a39cbdf7f4b881263dbf14533dcb58621d040a
Pavel Sinitski <pavel.sinitskiii@gmail.com>
2021-04-19 15:48:48 +0300

    Add Eslint

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,5 @@
 import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
-export const userName = readlineSync.question('May I have your name? ');
+const userName = readlineSync.question('May I have your name? ');
+export default userName;
